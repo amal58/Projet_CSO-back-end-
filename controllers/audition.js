458f3514b838a49533750c40e470d5b60034b072
unknown <simaasaad897@gmail.com>
2023-12-07 23:20:04 +0100

    envoyerMailCandidats

--- controllers/audition.js
+++ controllers/audition.js
@@ -1,5 +1,6 @@
 const Audition = require('../models/audition');
 const Personne = require('../models/personne');
+const nodemailer = require('nodemailer');
 
 exports.generateAuditions = async (req, res, next) => {
   try {
@@ -13,7 +14,6 @@ exports.generateAuditions = async (req, res, next) => {
 
     // Générer les auditions
     const auditions = [];
-
     let dateAudition = new Date(plageHoraireDebut);
 
     for (let i = 0; i < candidats.length; i += 2) {
@@ -41,6 +41,42 @@ exports.generateAuditions = async (req, res, next) => {
 
     // Enregistrer les auditions dans la base de données
     const createdAuditions = await Audition.insertMany(auditions);
+
+    // Configuration du transporteur nodemailer
+    const transporter = nodemailer.createTransport({
+      host: 'smtp-mail.outlook.com',
+      secureConnection:false,
+      port: 587,
+      tls:{
+        ciphers:'SSLv3',
+      },
+      auth: {
+        user: 'simaatest@outlook.com',
+        pass: 'SIMAA test2012',
+      },
+      // Ajoutez ces options pour augmenter le délai d'attente (en millisecondes)
+      connectionTimeout: 5000,
+      greetingTimeout: 10000,
+      socketTimeout: 15000,
+
+    });
+
+    const destinationEmail = 'hesemi6841@mcenb.com'; // Remplacez par votre adresse e-mail
+
+    // Envoi des e-mails d'invitation
+    for (const audition of createdAuditions) {
+      const candidat = await Personne.findById(audition.candidat);
+
+      const mailOptions = {
+        from: 'simaatest@outlook.com',
+        to: destinationEmail,
+        subject: 'Invitation à audition',
+        text: `Cher ${candidat.prenom},\n\nVous êtes invité(e) à participer à l'audition le ${audition.date} à ${audition.heureDebut}.\n\nCordialement,\nVotre équipe d'audition`,
+      };
+
+      // Envoyer l'e-mail
+      await transporter.sendMail(mailOptions);
+    }
 
     res.status(201).json({
       message: 'Auditions générées avec succès',
