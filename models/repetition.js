c35cd1484941c925d4cea08de2a96dbe18f017d2
unknown <lajiliamal218@gmail.com>
2023-12-10 14:56:55 +0100

    repetition crud

---
+++ models/repetition.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Joi = require('joi');
+const repetitionSchema = mongoose.Schema({
+   heureDebut:{type:String, required:true},
+   heureFin:{type:String, required:true},
+   date :{type:Date, required:true},
+   lieu:{type:String, required:true},
+   programme:{type:String,required:true},  
+   concert: { type: mongoose.Schema.Types.ObjectId, ref: 'Concert', required: true },
+});
+const repetitionValidationSchema = Joi.object({
+    heureDebut: Joi.string().pattern(/^([01]\d|2[0-3]):[0-5]\d$/).required(),
+    heureFin: Joi.string().pattern(/^([01]\d|2[0-3]):[0-5]\d$/).required(),
+    date: Joi.date().iso().required(),
+    lieu: Joi.string().required(),
+    programme: Joi.string().required(),
+  });
+
+const Repetition = mongoose.model("Repetition", repetitionSchema);
+module.exports = { Repetition, repetitionValidationSchema };
+
+
