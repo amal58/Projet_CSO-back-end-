f8d76460c37a1819b2d21ba73597235c0b163929
sirine-maatali <sirine.maatali.5@gmail.com>
2023-12-15 21:50:30 +0100

    schema concert

--- models/concert.js
+++ models/concert.js
@@ -4,19 +4,19 @@ const concertSchema = new mongoose.Schema({
   date: { type: Date, required: true , validate: { validator: dateValidator, message: 'La date doit être ultérieure à la date actuelle.' }},
   lieu: { type: String, required: true },
   affiche: { type: String },
-  programme: [{ type: mongoose.Schema.Types.ObjectId, ref:'Oeuvre' }],
-  repetition: [{ type: mongoose.Schema.Types.ObjectId, ref:'Repetition' }],
-  choristePC: [{ type: mongoose.Schema.Types.ObjectId, ref:'Choriste' }]
- 
+  programme: [{ type: mongoose.Schema.Types.ObjectId, ref:'Oeuvre' ,required:true}],
+  repetition: [{ type: mongoose.Schema.Types.ObjectId, ref:'Repetition',required:true }],
+  choriste: [{ type: mongoose.Schema.Types.ObjectId, ref:'Choriste' ,required : true }],
+  
 });
 
-// Validation personnalisée pour vérifier que la date est ultérieure à la date actuelle
-// function dateValidator(value) {
-//   const currentDate = new Date();
-//   return value > currentDate;
-// }
+// Validation pour vérifier que la date est ultérieure à la date actuelle
+function dateValidator(value) {
+  const currentDate = new Date();
+  return value > currentDate;
+}
 
-// Validation personnalisée pour vérifier l'unicité de la date et des choristes
+//Validation pour vérifier l'unicité de la date et des choristes
 concertSchema.path('date').validate(async function (value) {
   const existingConcert = await this.constructor.findOne({ date: value, choriste: { $in: this.choriste } });
   return !existingConcert;
