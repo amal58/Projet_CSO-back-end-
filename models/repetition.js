840134522cdce7d5998c402bfc649de8da8f05f6
unknown <lajiliamal218@gmail.com>
2023-12-19 14:01:57 +0100

    crud repe

---
+++ models/repetition.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Joi = require('joi');
+const repetitionSchema = mongoose.Schema({
+   heureDebut:{type:String, required:true},
+   heureFin:{type:String, required:true},
+   date :{type:Date, required:true},
+   lieu:{type:String, required:true},
+   programme:{type:String,required:true},   
+   concert: { type: mongoose.Schema.Types.ObjectId, ref: 'Concert', required: true },
+   choriste:[{type: mongoose.Schema.Types.ObjectId, ref: 'Choriste', required: true}],
+   urlQR:{type:String,required:true ,unique:true}, 
+});
+const repetitionValidationSchema = Joi.object({
+    heureDebut: Joi.string().pattern(/^([01]\d|2[0-3]):[0-5]\d$/).required(),
+    heureFin: Joi.string().pattern(/^([01]\d|2[0-3]):[0-5]\d$/).required(),
+    date: Joi.date().iso().required(),
+    lieu: Joi.string().required(),
+    programme: Joi.string().required(),
+    urlQR: Joi.string().required(),
+  });
+
+const Repetition = mongoose.model("Repetition", repetitionSchema);
+module.exports = { Repetition, repetitionValidationSchema };
---
+++ routes/repetition.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const repetitionController = require('../controllers/repetition');
+
+
+router.post('/', repetitionController.createRepetition );
+router.get("/concert/:id", repetitionController.getRepetitionbyconcert );
+
+
+module.exports = router;
