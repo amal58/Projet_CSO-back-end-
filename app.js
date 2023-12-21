87e2c9057f800ca0cae662c2cacbe6eabd4da97a
Abdi Safa <80724587+safa-abdi@users.noreply.github.com>
2023-12-21 04:16:16 +0100

    Lancer une audition en envoyant des emails au personnes ayant des emails vérifiées pour les permet de remplir le formulaire de candidature

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
-const express = require ("express")
-const app= express()
+const express = require("express");
+const bodyParser = require('body-parser');  // Importez bodyParser ici
+const app = express();
+const path = require('path');
 
-const OeuvreRoutes=require("./routes/oeuvre")
-//const personneRoutes = require('./routes/personne');
+// Configuration du moteur de modèle EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-const mongoose = require('mongoose')
-const concertRoutes=require("./routes/concert");
+
+const OeuvreRoutes = require("./routes/oeuvre");
+const mongoose = require('mongoose');
+const concertRoutes = require("./routes/concert");
 const repetitionRoutes = require('./routes/repetition');
+const ValidMailPRoutes = require('./routes/validerMailPersonne');
+const auditionRoutes = require('./routes/audition');
 
-mongoose
-.connect('mongodb+srv://p92934700:7RoxD6S97xxp1Dea@cluster0.peomj36.mongodb.net/projetCSO', {
+mongoose.connect('mongodb+srv://p92934700:7RoxD6S97xxp1Dea@cluster0.peomj36.mongodb.net/projetCSO', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB", error);
-  });
-
-  app.use(express.json())
-  app.use("/Oeuvre",OeuvreRoutes)
-  app.use("/api/concert",concertRoutes)
-  app.use('/api/repetitions', repetitionRoutes);
-
-  //app.use('/api/candidats', personneRoutes);
+.then(() => {
+  console.log("Connected to MongoDB");
+})
+.catch((error) => {
+  console.error("Error connecting to MongoDB", error);
+});
 
 
 
-module.exports=app
+// Utilisez bodyParser ici avant les app.use
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/Oeuvre", OeuvreRoutes);
+app.use("/api/concert", concertRoutes);
+app.use('/api/repetitions', repetitionRoutes);
+app.use('/validerMail', ValidMailPRoutes);
+app.use('/api/auditions', auditionRoutes); 
+
+
+
+
+module.exports = app;
--- models/personne.js
+++ models/personne.js
@@ -1,24 +1,23 @@
-// const mongoose = require("mongoose");
-// const mongoosePaginate = require('mongoose-paginate-v2');
+const mongoose = require("mongoose");
+const Joi = require('joi');
+const personneSchema = mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  nom: { type: String, required: true },
+  prenom: { type: String, required: true },
+  nomJeuneFille: { type: String },
+  sexe: { type: String, enum: ['Homme', 'Femme'], required: true },
+  dateNaissance: { type: Date, required: true },
+  Nationalite: { type: String, required: true },
+  taille: { type: Number, required: true },
+  telephone: { type: String, required: true },
+  cin: { type: String, required: true },
+  situationPro: { type: String, required: true },
+  createdAt: { type: Date,default: Date.now },
+  role: {
+    type: String,
+    enum: ['manager', 'admin', 'choriste', 'candidat'],
+    default:'candidat'
+  },
+});
 
-// const personneSchema = mongoose.Schema({
-//   email: { type: String, required: true, unique: true },
-//   nom: { type: String, required: true },
-//   prenom: { type: String, required: true },
-//   nomJeuneFille: { type: String },
-//   sexe: { type: String, enum: ['Homme', 'Femme'], required: true },
-//   dateNaissance: { type: Date, required: true },
-//   Nationalite: { type: String, required: true },
-//   taille: { type: Number, required: true },
-//   telephone: { type: String, required: true },
-//   cin: { type: String, required: true },
-//   situationPro: { type: String, required: true },
-//   role: {
-//     type: String,
-//     enum: ['manager', 'admin', 'choriste', 'candidat'],
-//     required: true,
-    
-//   },
-// });
-// personneSchema.plugin(mongoosePaginate);
-// module.exports = mongoose.model("Personne", personneSchema);
+module.exports = mongoose.model("Personne", personneSchema);
