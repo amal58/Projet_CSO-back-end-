4f23360c1a8624e8f59cf1c0f7173e9d6549fc24
unknown <lajiliamal218@gmail.com>
2023-12-21 12:12:55 +0100

    schema personne

--- models/choriste.js
+++ models/choriste.js
@@ -1,17 +1,22 @@
 const mongoose = require("mongoose");
-
 const choristeSchema = mongoose.Schema({
-    choriste: {
+    candidatId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Personne',
+        ref: 'CandAud',
         required: true,
       },
-    status: { type: String, required: true, enum: ['inactif', 'junior', 'senior', 'veteran', 'eliminer'] },
-    typePupitre: { type: String, enum: ['Base', 'Alto', 'Tenor', 'Soprano'], required: true },
-    chefpupitre:{type:Boolean, required:true},
-    chefchoeur:{type:Boolean,required:true},
+    nominé: { type: Boolean, default: false },
+    éliminé: { type: Boolean, default: false },
+    raisonÉlimination: { type: String },
+    chefpupitre:{type:Boolean, required:true ,default:false},  
+    chefchoeur:{type:Boolean,required:true,default:false},
+    historiqueStatut: [
+      {
+        saison: { type: String,required:true },
+        statut: { type: String, required: true, enum: ['choriste','junior', 'senior', 'veteran', 'inactif'] },
+        }],
+        compte: { type: mongoose.Schema.Types.ObjectId, ref: 'Compte', required: true },
+      })
 
-});
-module.exports = mongoose.model("Choriste",choristeSchema);
-
+  module.exports = mongoose.model("Choriste",choristeSchema);
 
