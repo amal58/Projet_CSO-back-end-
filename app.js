213374019822d57a98c82b561ebfd3cc33536118
Abdi Safa <80724587+safa-abdi@users.noreply.github.com>
2023-12-26 04:12:30 +0100

    Last commit

--- app.js
+++ app.js
@@ -3,15 +3,9 @@ const bodyParser = require('body-parser');  // Importez bodyParser ici
 const app = express();
 const path = require('path');
 
-// Configuration du moteur de modèle EJS
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
 
-const OeuvreRoutes = require("./routes/oeuvre");
 const mongoose = require('mongoose');
-const concertRoutes = require("./routes/concert");
-const repetitionRoutes = require('./routes/repetition');
 const ValidMailPRoutes = require('./routes/validerMailPersonne');
 const auditionRoutes = require('./routes/audition');
 
@@ -31,9 +25,6 @@ mongoose.connect('mongodb+srv://p92934700:7RoxD6S97xxp1Dea@cluster0.peomj36.mong
 // Utilisez bodyParser ici avant les app.use
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/Oeuvre", OeuvreRoutes);
-app.use("/api/concert", concertRoutes);
-app.use('/api/repetitions', repetitionRoutes);
 app.use('/validerMail', ValidMailPRoutes);
 app.use('/api/auditions', auditionRoutes); 
 
