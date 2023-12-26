c30f731f7107c0a3b5f07e77c92d46d0b53d61fd
unknown <lajiliamal218@gmail.com>
2023-12-26 02:21:23 +0100

    add conge add notif  get list notiffF

--- controllers/conge.js
+++ controllers/conge.js
@@ -1,14 +1,13 @@
 var path = require('path');
 const schedule = require('node-schedule');
+const http = require('http');
 const Choriste = require('../models/choriste');
+const Personne = require('../models/personne');
 const Conge= require('../models/conge');
-
-const io = require('socket.io');
-
+const io = require('socket.io')(http);
 
 exports.AjouterConge = async (req, res) =>{
     try{
-  
         const conge = new Conge({
             dateDebutConge: req.body.dateDebutConge,
             dateFinConge: req.body.dateFinConge,
@@ -17,10 +16,11 @@ exports.AjouterConge = async (req, res) =>{
         })
        const resultat = await  conge.save();
 
-       const choriste=  await Choriste.findById({_id: req.params.id}).populate("candidatId")
+       const choriste=  await Choriste.findOne({_id: req.params.id}).populate("candidatId")
+   
        if(resultat){
             io.emit("new_notification",`vous avez une nouvelle congée de ${choriste.candidatId.nom} ${choriste.candidatId.prenom}`)
-    }
+     }
   
     res.status(200).json({
         message: "bien retourner",
@@ -33,22 +33,15 @@ console.log(e);
 }
 
 
-
-
-
- 
-    // schedule.scheduleJob('* * * * *', async()=>{
- 
-    //  try {
-    //      const tab = [];
-    //      const existcongee =  await Conge.find({enConge: false}).populate("Choriste") ;
-    //  console.log(existcongee);
-    //  socket.emit("list",existcongee)
-         
-    //  } catch (e) {
-        
-    //      console.error("Une erreur s'est produite :", e);
-    //  }
-    // })
+    schedule.scheduleJob('* * * * *', async()=>{
+     try {
+         const tab = [];
+         const existcongee =  await Conge.find({etat:'en attente'}).populate("Choriste");
+         console.log(existcongee);
+         socket.emit("list",existcongee)
+     } catch (e) {
+         console.error("Une erreur s'est produite :", e);
+     }
+    })
  
  
