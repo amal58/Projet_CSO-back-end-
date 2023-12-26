7f1e3d7b1a339f31bd7bfd25c5f339f9910351dc
Abdi Safa <80724587+safa-abdi@users.noreply.github.com>
2023-12-26 23:29:15 +0100

    last commit 1

--- routes/participantsRoutes.js
+++ routes/participantsRoutes.js
@@ -7,6 +7,5 @@ router.get('/:tessit/:idC', participantsController.getAll);
 router.get('/:concertId', participantsController.getParticipantsByConcertId);
 router.get('/participants/:idC/:pourcentage', participantsController.getListeParticipants);
 router.get('/participants/absence/:idC/:pourcentage', participantsController.getListeParticipantsParAbsence);
-//router.get('/pupitre/:pupit/:idC/:pourcentage', participantsController.getListeParticipantsByPupitre);
 
 module.exports = router;
