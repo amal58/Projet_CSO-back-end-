4e6b0e877a1993638d56a4fc3d0b95cbba826ee9
Abdi Safa <80724587+safa-abdi@users.noreply.github.com>
2023-12-26 02:15:47 +0100

    remove files absencepresence from this branch

--- routes/absencepresence.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const presenceController = require('../controllers/absencepresence');
-
-
-router.post('/', presenceController.createPresence );
-
-module.exports = router;
