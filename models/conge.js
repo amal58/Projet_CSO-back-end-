af7ca5be823f5b591e3e3bf88f739e6daae6ab99
unknown <lajiliamal218@gmail.com>
2024-01-16 18:40:46 +0100

    final

--- models/conge.js
+++ models/conge.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Joi = require("joi");
+
 
 const congeSchema = new mongoose.Schema({
   dateDebutConge:{ type: Date, required: true },
@@ -8,13 +8,6 @@ const congeSchema = new mongoose.Schema({
   choriste: { type: mongoose.Schema.Types.ObjectId, ref: 'Choriste', required: true },
 });
 
-// Schéma de validation avec Joi
-// const congeValidationSchema = Joi.object({
-//   dateDebutConge: Joi.date().required(),
-//   dateFinConge: Joi.date().required(),
-//   etat: Joi.string().valid('en attente', 'accepte').default('en attente'),
-//   choriste: Joi.string().required(), 
-// });
 
 
 module.exports = mongoose.model("Conge", congeSchema);
