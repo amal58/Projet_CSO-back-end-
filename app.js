1df29a2bd8623b837937a1297cfdeb1e08777613
unknown <lajiliamal218@gmail.com>
2024-01-16 19:14:55 +0100

    finalL

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
 const express = require ("express")
 const app= express()
 const mongoose = require('mongoose')
-const repetitionRoutes = require('./routes/repetition');
-const congeRoutes = require('./routes/conge');
-const candidatRoutes = require('./routes/candidat');
-const choristeRoutes = require('./routes/choriste');
+
+
 const abprRoutes = require('./routes/absencepresence');
-const cron = require('node-cron');
-const congeController = require('./controllers/conge');
-
-
-
-//'*/2 * * * * *' chaque 2 secondes
-// Exécuter la fonction toutes les 2 HEURES
-cron.schedule('0 */2 * * *', () => {
-  console.log('Exécution du job cron toutes les 2 SECONDES pour envoyer des notifications de congé.');
-  congeController.processCongeNotifications();
-});
-
-
 
 
 
@@ -37,11 +22,8 @@ mongoose
 
   app.use(express.json())
 
-  app.use('/api/repetitions', repetitionRoutes);
-  app.use('/api/conge', congeRoutes);
   
-  app.use('/api/candidats', candidatRoutes);
-  app.use('/api/choristes', choristeRoutes);
+  
 
   app.use('/api/absencepresence', abprRoutes);
 
