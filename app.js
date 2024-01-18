845b7ce16a333da4c3ecb153e682acebaf2f13cc
unknown <lajiliamal218@gmail.com>
2024-01-18 15:51:34 +0100

    tache repecrudd

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const express = require ("express")
 const app= express()
 const mongoose = require('mongoose')
+const repetitionRoutes = require('./routes/repetition');
+
 
 mongoose
-.connect("mongodb://127.0.0.1:27017/database", {
+.connect("mongodb://127.0.0.1:27017/data", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB", error);
+  });
+
+  
+  app.use(express.json())
+
+  app.use('/api/repetitions', repetitionRoutes);
+  
+
+module.exports=app
+const express = require ("express")
+const app= express()
+const mongoose = require('mongoose')
+const candARoutes=require('./routes/candidatAudition')
+const auditionRoutes = require('./routes/audition');
+const personneRoutes = require('./routes/personne');
+const choristeRoutes = require('./routes/choriste');
+const Choriste = require('./models/choriste');
+const bcrypt=require('bcryptjs')
+const connection=async()=>{
+try{
+await mongoose
+      .connect("mongodb://127.0.0.1:27017/data", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -47,7 +79,9 @@ connection()
 
   
   app.use(express.json())
-
-
+  app.use('/api/auditions', auditionRoutes); 
+  app.use("/api/cand",candARoutes)
+  app.use('/api/candidats', personneRoutes);
+  app.use('/api/choriste', choristeRoutes);
 
 module.exports=app
