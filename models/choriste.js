2ce708bac9f732f32ada64ca4655e7e783e3a14e
sirine-maatali <sirine.maatali.5@gmail.com>
2024-01-18 22:20:42 +0100

    Sauvegarde des modifications locales

--- models/choriste.js
+++ models/choriste.js
@@ -3,16 +3,15 @@ const mongoose = require("mongoose");
 const choristeSchema = new mongoose.Schema({
     candidatId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'CandAud',
+        ref: 'Personne',
       },
     role:{type:String , enum:['admin' , 'choriste' , 'Manager','chefpupitre','chefchoeur']},
     login:{type:String},
     statutAcutel:{type:String, enum: ['choriste','junior', 'senior', 'veteran', 'inactif']},
-    typepupitre:{type:String,required:true},
     historiqueStatut: [
       {
-        saison: { type:Number,required:true },
-        statut: { type: String, enum: ['choriste','junior', 'senior', 'veteran', 'inactif'] },
+        saison: { type:Number },
+        statut: { type: String },
         }],
     password:{type:String,required:true},
     confirmationStatus: { type: String, default: 'En attente de confirmation' },
